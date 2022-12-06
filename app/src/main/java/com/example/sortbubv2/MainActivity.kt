75b9c314bf55a0f0a116ac9bca8fa582bcaeb0f3
student-14 <terleckawika123@gmail.com>
2022-12-06 10:02:58 +0100

    Losowanie

--- app/src/main/java/com/example/sortbubv2/MainActivity.kt
+++ app/src/main/java/com/example/sortbubv2/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.sortbubv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +21,32 @@ class MainActivity : AppCompatActivity() {
                         arr[i + 1] = temp
                         swap = true
                     }
+
                 }
             }//
                 }
+        val s1 = findViewById<Switch>(R.id.switch1)
+        val s2 = findViewById<Switch>(R.id.switch2)
+        val s3 = findViewById<Switch>(R.id.switch3)
+        val s4 = findViewById<Switch>(R.id.switch4)
+        val s5 = findViewById<Switch>(R.id.switch5)
+        val s6 = findViewById<Switch>(R.id.switch6)
+        val s7 = findViewById<Switch>(R.id.switch7)
+        val s8 = findViewById<Switch>(R.id.switch8)
+        val s9 = findViewById<Switch>(R.id.switch9)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            s1.text = Random.nextInt(0,9).toString()
+            s2.text = Random.nextInt(0,9).toString()
+            s3.text = Random.nextInt(0,9).toString()
+            s4.text = Random.nextInt(0,9).toString()
+            s5.text = Random.nextInt(0,9).toString()
+            s6.text = Random.nextInt(0,9).toString()
+            s7.text = Random.nextInt(0,9).toString()
+            s8.text = Random.nextInt(0,9).toString()
+            s9.text = Random.nextInt(0,9).toString()
+        }
+
             }
         }
 
